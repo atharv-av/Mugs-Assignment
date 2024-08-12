624ef982ecbefa11a9999e0303e2b1a9a47b7680
atharv-av <atharvv333@gmail.com>
2024-08-13 02:54:57 +0530

    created navigation bar

---
+++ pages/Portfolio.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+const Portfolio = () => {
+  return (
+    <div className="dark:bg-black">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Portfolio;
